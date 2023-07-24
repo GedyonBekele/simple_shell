c9baf59f555069f21b935e82adbeac94414945a8
GedyonBekele <gedyonbekele@gmail.com>
2023-07-24 08:48:07 -0700

    update

--- exits.c
+++ exits.c
@@ -31,7 +31,7 @@ return (s);
 }
 
 /**
- **_strncat - concatenates two strings
+ *_strncat - concatenates two strings
  *@dest: the first string
  *@src: the second string
  *@n: the amount of bytes to be maximally used
@@ -57,3 +57,18 @@ dest[i] = '\0';
 return (s);
 }
 
+/**
+ *_strchr - locates a character in a string
+ *@s: the string to be parsed
+ *@c: the character to look for
+ *Return: (s) a pointer to the memory area s
+ */
+char *_strchr(char *s, char c)
+{
+do {
+if (*s == c)
+return (s);
+} while (*s++ != '\0');
+
+return (NULL);
+}
