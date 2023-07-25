669da12cd66319d1b24e1de282a44b25d20593f9
GedyonBekele <gedyonbekele@gmail.com>
2023-07-25 07:49:44 -0700

    update

--- getLine.c
+++ getLine.c
@@ -43,3 +43,48 @@ info->cmd_buf = buf;
 }
 return (r);
 }
+
+/**
+ * get_input - gets a line minus the newline
+ * @info: parameter struct
+ *
+ * Return: bytes read
+ */
+ssize_t get_input(info_t *info)
+{
+static char *buf; /* the ';' command chain buffer */
+static size_t i, j, len;
+ssize_t r = 0;
+char **buf_p = &(info->arg), *p;
+
+_putchar(BUF_FLUSH);
+r = input_buf(info, &buf, &len);
+if (r == -1) /* EOF */
+return (-1);
+if (len) /* we have commands left in the chain buffer */
+{
+j = i; /* init new iterator to current buf position */
+p = buf + i; /* get pointer for return */
+
+check_chain(info, buf, &j, i, len);
+while (j < len) /* iterate to semicolon or end */
+{
+if (is_chain(info, buf, &j))
+break;
+j++;
+}
+
+i = j + 1;
+if (i >= len)
+{
+i = len = 0;
+info->cmd_buf_type = CMD_NORM;
+}
+
+*buf_p = p;
+return (_strlen(p));
+}
+
+*buf_p = buf;
+return (r);
+}
